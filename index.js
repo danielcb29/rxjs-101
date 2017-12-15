839821fea441651868af3a1167f7ff9e47f19546
Krishna Palem <krishna.palem@autodesk.com>
2017-12-15 11:35:35 -0500

    mousemove observable

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ var observable = Rx.Observable.create(observer => {
     observer.next('Hello World');
 });
 
-
 observable.subscribe(result => {
     let box = document.getElementById('box');
     box.innerHTML = result;
     console.log(result);
 }); 
+
+var mouseMove = Rx.Observable.fromEvent(document, "mousemove");
+var text = "RxJs experiment";
+var container = document.getElementById('container');
+
+for(var i=0; i <text.length; i++){
+    (function(i){
+        var s = document.createElement("span");
+        s.innerHTML = text.charAt(i);
+        s.style.position = "absolute";
+        container.appendChild(s);
+
+        mouseMove.delay(i*100).subscribe(function(mouseEvent){
+            s.style.top = mouseEvent.clientY + "px";
+            s.style.left = mouseEvent.clientX + i * 10 + 15 + "px";
+        });
+    })(i);
+}
