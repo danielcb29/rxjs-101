86dd700a0312e10aa8cf5a20a8078dfa464b36ed
Krishna Palem <krishna.palem@autodesk.com>
2017-12-18 14:30:09 -0500

    Array event example

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ var observable = Rx.Observable.create(observer => {
 });
 
 observable.subscribe(result => {
-    let box = document.getElementById('box');
+    let box = document.getElementById('1');
     box.innerHTML = result;
     console.log(result);
 }); 
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+// A stream
+var numbers$ = Rx.Observable
+  .interval(500)
+  .take(10)
+  .map(i => numbers[i]);
+
+numbers$.subscribe(n => {
+    let box = document.getElementById('2');
+    box.innerHTML = n;
+    console.log(n);
+});
 
 var mouseMove = Rx.Observable.fromEvent(document, "mousemove");
 var text = "RxJs experiment";
