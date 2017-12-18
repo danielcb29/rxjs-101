5d3d6a2e2e731268d5785eb4150b7d5c836c3c3d
Daniel <daniel.correa@autodesk.com>
2017-12-18 15:14:33 -0500

    Add webpack setup and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+let path = require('path')
+
+module.exports = {
+    entry: './index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+
+};
