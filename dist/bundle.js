a540e0d95402e6ff9a828c077de3fb7e48d66fce
Daniel <daniel.correa@autodesk.com>
2017-12-29 14:48:26 -0500

    Mouse event review

--- dist/bundle.js
+++ dist/bundle.js
@@ -25623,24 +25623,24 @@ const runHelloWorld = () => {
 
 
 const runMouseEvent = () => {
-    var mouseMove = __WEBPACK_IMPORTED_MODULE_0_rxjs_Rx___default.a.Observable.fromEvent(document, "mousemove");
-    var text = "RxJs experiment";
-    var container = document.getElementById('mouseEvent-container');
+  const mouseMove = __WEBPACK_IMPORTED_MODULE_0_rxjs_Rx___default.a.Observable.fromEvent(document, "mousemove");
+  const text = "RxJs experiment";
+  const container = document.getElementById("mouseEvent-container");
 
-    for (var i = 0; i < text.length; i++) {
-        (function (i) {
-            var s = document.createElement("span");
-            s.innerHTML = text.charAt(i);
-            s.style.position = "absolute";
-            s.style.color = "#191970";
-            container.appendChild(s);
+  for (let i = 0; i < text.length; i++) {
+    (function (i) {
+      let s = document.createElement("span");
+      s.innerHTML = text.charAt(i);
+      s.style.position = "absolute";
+      s.style.color = "#191970";
+      container.appendChild(s);
 
-            mouseMove.delay(i * 100).subscribe(function (mouseEvent) {
-                s.style.top = mouseEvent.clientY + "px";
-                s.style.left = mouseEvent.clientX + i * 10 + 15 + "px";
-            });
-        })(i);
-    }
+      mouseMove.delay(i * 100).subscribe(function (mouseEvent) {
+        s.style.top = mouseEvent.clientY + "px";
+        s.style.left = mouseEvent.clientX + i * 10 + 15 + "px";
+      });
+    })(i);
+  }
 };
 
 
--- src/mouseEvent.js
+++ src/mouseEvent.js
@@ -1,26 +1,24 @@
-import Rx from 'rxjs/Rx';
+import Rx from "rxjs/Rx";
 
 const runMouseEvent = () => {
-    var mouseMove = Rx.Observable.fromEvent(document, "mousemove");
-    var text = "RxJs experiment";
-    var container = document.getElementById('mouseEvent-container');
+  const mouseMove = Rx.Observable.fromEvent(document, "mousemove");
+  const text = "RxJs experiment";
+  const container = document.getElementById("mouseEvent-container");
 
-    for(var i=0; i <text.length; i++){
-        (function(i){
-            var s = document.createElement("span");
-            s.innerHTML = text.charAt(i);
-            s.style.position = "absolute";
-            s.style.color = "#191970";
-            container.appendChild(s);
+  for (let i = 0; i < text.length; i++) {
+    (function(i) {
+      let s = document.createElement("span");
+      s.innerHTML = text.charAt(i);
+      s.style.position = "absolute";
+      s.style.color = "#191970";
+      container.appendChild(s);
 
-            mouseMove.delay(i*100).subscribe(function(mouseEvent){
-                s.style.top = mouseEvent.clientY + "px";
-                s.style.left = mouseEvent.clientX + i * 10 + 15 + "px";
-            });
-        })(i);
-    }
-}
+      mouseMove.delay(i * 100).subscribe(function(mouseEvent) {
+        s.style.top = mouseEvent.clientY + "px";
+        s.style.left = mouseEvent.clientX + i * 10 + 15 + "px";
+      });
+    })(i);
+  }
+};
 
-export {
-    runMouseEvent
-}
+export { runMouseEvent };
